636a90eccb29aaac336e15e072b641f6ac5ce4f2
Patrick Castro <patrick.gomes@corebiz.com.br>
2019-07-05 12:20:26 -0300

    Geral: Correções no Package e Gulp File

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,10 @@ const browserify = require('gulp-browserify');
 const browserSync   = require('browser-sync').create();
 // const del = require('del');
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
+const svgmin = require('gulp-svgmin');
 const uglify = require('gulp-uglify');
 
 const paths = {
@@ -24,13 +26,24 @@ const paths = {
     dest: './dist/assets/css/',
     srcWatch: 'src/assets/scss/**/*.scss',
   },
+  imgs: {
+    src: 'src/assets/img/*.{png,gif,jpg}',
+    dest: './dist/assets/img/',
+    srcWatch: 'src/assets/img/*'
+  },
+  svgs: {
+    src: 'src/assets/img/icon/*.svg',
+    dest: './dist/assets/img/icon/',
+    srcWatch: 'src/assets/img/icon/*'
+  },
   scripts: {
     src: 'src/assets/js/common/*.js',
     dest: './dist/assets/js/',
     srcWatch: 'src/assets/**/*.js',
   },
-  htmls: {
-    src: 'src/views/common/*/*.pug',
+  markup: {
+    pug: 'src/views/common/*/*.pug',
+    html: 'src/views/html/*.html',
     dest: './dist/views/html',
   }
 };
@@ -42,19 +55,34 @@ const paths = {
 
 const pugtranspile = () => {
   return gulp.src([
-    paths.htmls.src,
+    paths.markup.pug,
     'src/views/common/_layouts/*.pug',
     'src/views/common/_partials/*.pug',
   ])
     .pipe(pug({
       pretty: true
     }))
-    .pipe(gulp.dest(paths.htmls.dest));
+    .pipe(gulp.dest(paths.markup.dest));
 }
 const htmls = () => {
-  return gulp.src('src/views/html/*.html')
-    .pipe(gulp.dest(paths.htmls.dest));
+  return gulp.src(paths.markup.html)
+    .pipe(gulp.dest(paths.markup.dest));
 }
+
+const minimg = () => {
+  return gulp.src(paths.imgs.src)
+      .pipe(imagemin())
+      .pipe(gulp.dest(paths.imgs.dest))
+      .pipe(gulp.dest('./dist/vtex_speed'));
+};
+
+const minsvg = () => {
+  return gulp.src(paths.svgs.src)
+      .pipe(svgmin())
+      .pipe(gulp.dest(paths.svgs.dest))
+      .pipe(gulp.dest('./dist/vtex_speed'));
+};
+
 const styles = () => {
   return gulp.src(paths.styles.src)
     .pipe(sass({outputStyle: 'compressed'}))
@@ -105,10 +133,12 @@ const watch = () => {
   gulp.watch(paths.scripts.srcWatch, scripts).on('change',browserSync.reload);
   gulp.watch('src/views/**/*', pugtranspile).on('change',browserSync.reload);
   gulp.watch('src/views/html/*', htmls).on('change',browserSync.reload);
+  gulp.watch(paths.imgs.srcWatch, minimg).on('change',browserSync.reload);
+  gulp.watch(paths.svgs.srcWatch, minsvg).on('change',browserSync.reload);
 }
 
 //------------------------------ Tasks -----------------------------
-const build = gulp.series(gulp.parallel(sync,styles, scripts, htmls, pugtranspile, watch));
+const build = gulp.series(gulp.parallel(sync, minimg, minsvg, styles, scripts, htmls, pugtranspile, watch));
 
 // exports.pluginsJs = pluginsJs;
 // exports.clean = clean;
@@ -116,6 +146,8 @@ exports.styles = styles;
 exports.scripts = scripts;
 exports.htmls = htmls;
 exports.pugtranspile = pugtranspile;
+exports.minimg = minimg;
+exports.minsvg = minsvg;
 exports.watch = watch;
 exports.build = build;
 
