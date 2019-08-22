1f6de0ea0176c11a4511c98c016c4bb7da3d92e1
Patrick Castro <patrick.gomes@corebiz.com.br>
2019-08-22 15:47:14 -0300

    Revert para b0b705a9b2d6d62149f2d9bf721c509d8add0463

--- backup/backup-02-08-2019/assets/js/QDB-LP-Menu-de-makes.js
+++
@@ -1,1 +0,0 @@
-"use strict";function checkDevice(){var i=void 0;i=768<window.innerWidth?3:1.5,$(".makeup--container__content--slick").slick({adaptiveHeight:!0,arrows:!0,draggable:!0,slidesToShow:i,infinite:!1})}checkDevice();
--- node-tasks.js
+++ node-tasks.js
@@ -1,65 +1,58 @@
-const Backup = (day, hStart, hEnd) =>{
-    var schedule = require('node-schedule');
-    var ncp = require('ncp').ncp;
-    var fs = require('fs');
-    var path = require('path');
-    var rimraf = require('rimraf');
-    
-    var date = new Date();
-    var dd = date.getDate();
-    var mm = date.getMonth()+1; //As January is 0.
-    var yyyy = date.getFullYear();
-    var curday = function(sp){
-        if(dd<10) dd='0'+dd;
-        if(mm<10) mm='0'+mm;
-        return (dd+sp+mm+sp+yyyy);
-    };
-    // CREATE BACKUP FILES
-    if(date.getDay() == day){
-        var rule = new schedule.RecurrenceRule();
-        var dir = "backup";
-    
-        console.log("Backup begin " + date.getHours()) + " hours";
-        rule.dayOfWeek = day;
-        rule.hour = new schedule.Range(hStart, hEnd);
-        rule.minute = new schedule.Range(0, 59);
-    
-        var j = schedule.scheduleJob(rule, function(){
-            console.log("scheduled");
-            fs.readdir(dir, function(err, files) {
-                files.forEach(function(file, index) {
-                    fs.stat(path.join(dir, file), function(err, stat) {
-                    var endTime, now;
+var schedule = require('node-schedule');
+var ncp = require('ncp').ncp;
+var fs = require('fs');
+var path = require('path');
+var rimraf = require('rimraf');
+
+var date = new Date();
+var dd = date.getDate();
+var mm = date.getMonth()+1; //As January is 0.
+var yyyy = date.getFullYear();
+var curday = function(sp){
+    if(dd<10) dd='0'+dd;
+    if(mm<10) mm='0'+mm;
+    return (dd+sp+mm+sp+yyyy);
+};
+// CREATE BACKUP FILES
+if(date.getDay() == 6){
+    var rule = new schedule.RecurrenceRule();
+    var dir = "backup";
+
+    console.log("Backup begin " + date.getHours()) + " hours";
+    rule.dayOfWeek = 6;
+    rule.hour = new schedule.Range(0, 23);
+    rule.minute = new schedule.Range(0, 59);
+
+    var j = schedule.scheduleJob(rule, function(){
+        
+        fs.readdir(dir, function(err, files) {
+            files.forEach(function(file, index) {
+                fs.stat(path.join(dir, file), function(err, stat) {
+                var endTime, now;
+                if (err) {
+                    return console.error(err);
+                }
+                now = new Date().getTime();
+                endTime = new Date(stat.ctime).getTime() + 2419200000;
+                if (now > endTime) {
+                    return rimraf(path.join(dir, file), function(err) {
                     if (err) {
                         return console.error(err);
                     }
-                    now = new Date().getTime();
-                    endTime = new Date(stat.ctime).getTime() + 2419200000;
-                    if (now > endTime) {
-                        console.log("file created");
-                        return rimraf(path.join(dir, file), function(err) {
-                        if (err) {
-                            return console.error(err);
-                        }
-                        console.log('successfully deleted');
-                        });
-                    }
+                    console.log('successfully deleted');
                     });
+                }
                 });
             });
-            // !fs.existsSync(dir + "/backup-" + curday('-')) ? fs.mkdirSync(dir + "/backup-" + curday('-')) : '';
-    
-            ncp("dist", dir + "/backup-" + curday('-'), function (err) {
-                if (err) {
-                  return console.error(err);
-                }
-                console.log('Backup Done!');
-                j.cancel();
-            });
         });
-    }
-}
+        // !fs.existsSync(dir + "/backup-" + curday('-')) ? fs.mkdirSync(dir + "/backup-" + curday('-')) : '';
 
-module.exports.init = function () {
-    Backup("5", 8, 21);
+        ncp("dist", dir + "/backup-" + curday('-'), function (err) {
+            if (err) {
+              return console.error(err);
+            }
+            console.log('Backup Done!');
+            j.cancel();
+        });
+    });
 }
