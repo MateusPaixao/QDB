4dae2cf3afe520564b0fafcd5581a375c384f486
Patrick Castro <patrick.gomes@corebiz.com.br>
2019-08-24 16:41:13 -0300

    Service Worker e Home

---
+++ src/assets/js/common/QDBCommon-Home.js
@@ -0,0 +1,3 @@
+import Home from "./modules/Home/home-index"
+
+Home.init();
---
+++ src/assets/js/common/modules/Home/home-index.js
@@ -0,0 +1,11 @@
+import Hero from "./components/hero-banner.jsx"
+
+const Methods = {
+    init(){
+
+    }
+}
+
+export default {
+    init: Methods.init
+}
