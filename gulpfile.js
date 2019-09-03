60d200d6e710457ae26b8c6b6040d78144415bc6
Patrick Castro <patrick.gomes@corebiz.com.br>
2019-09-03 14:02:44 -0300

    Configurado tarefa do checkout no Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const storeName = 'qbbr';
 
 const bases = {
     src: './src',
-    build: './dist/vtex_speed'
+    build: './dist/vtex_speed/arquivos/'
 }
 
 //----------------------------- Modules ---------------------------------
@@ -22,12 +22,6 @@ const uglify = require('gulp-uglify');
 const rename = require("gulp-rename");
 
 const paths = {
-  styles: {
-    src: 'src/assets/scss/common/*.scss',
-    dest: './dist/assets/css/',
-    qa: './qa/assets/css/',
-    srcWatch: 'src/assets/scss/**/*.scss',
-  },
   imgs: {
     src: 'src/assets/img/*.{png,gif,jpg}',
     dest: './dist/assets/img/',
@@ -38,11 +32,17 @@ const paths = {
     dest: './dist/assets/img/icon/',
     srcWatch: 'src/assets/img/icon/*'
   },
+  styles: {
+    src: ['src/assets/scss/common/*.scss', '!src/assets/scss/common/checkout6-custom.scss'],
+    dest: './dist/assets/css/',
+    qa: './qa/assets/css/',
+    srcWatch: ['src/assets/scss/**/*.scss', '!src/assets/scss/**/checkout6-custom.scss'],
+  },
   scripts: {
-    src: ['src/assets/js/common/*.js', 'src/assets/js/common/*.jsx'],
+    src: ['src/assets/js/common/*.js', 'src/assets/js/common/*.jsx', '!src/assets/js/common/checkout6-custom.js'],
     dest: './dist/assets/js/',
     qa: './qa/assets/js/',
-    srcWatch: ['src/assets/**/*.js','src/assets/**/*.jsx'],
+    srcWatch: ['src/assets/**/*.js','src/assets/**/*.jsx', '!src/assets/**/checkout6-custom.js'],
   },
   markup: {
     pug: 'src/views/common/*/*.pug',
@@ -76,14 +76,14 @@ const minimg = () => {
   return gulp.src(paths.imgs.src)
       .pipe(imagemin())
       .pipe(gulp.dest(paths.imgs.dest))
-      .pipe(gulp.dest('./dist/vtex_speed'));
+      .pipe(gulp.dest('./dist/vtex_speed/arquivos/'));
 };
 
 const minsvg = () => {
   return gulp.src(paths.svgs.src)
       .pipe(svgmin())
       .pipe(gulp.dest(paths.svgs.dest))
-      .pipe(gulp.dest('./dist/vtex_speed'));
+      .pipe(gulp.dest('./dist/vtex_speed/arquivos/'));
 };
 
 const styles = () => {
@@ -94,7 +94,7 @@ const styles = () => {
       cascade: false,
     }))
     .pipe(gulp.dest(paths.styles.dest))
-    .pipe(gulp.dest('./dist/vtex_speed'))
+    .pipe(gulp.dest('./dist/vtex_speed/arquivos/'))
     .pipe(rename(function (path) {
       path.basename = "QA-" + path.basename
     }))
@@ -107,11 +107,31 @@ const scripts = () => {
     .pipe(browserify())
     .pipe(uglify())
     .pipe(gulp.dest(paths.scripts.dest))
-    .pipe(gulp.dest('./dist/vtex_speed'))
+    .pipe(gulp.dest('./dist/vtex_speed/arquivos/'))
     .pipe(rename(function (path) {
       path.basename = "QA-" + path.basename
     }))
     .pipe(gulp.dest(paths.scripts.qa));
+}
+
+const checkoutStyles = () => {
+  return gulp.src("src/assets/scss/common/checkout6-custom.scss")
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({
+      browsers: ['last 10 versions'],
+      cascade: false,
+    }))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(gulp.dest('./dist/vtex_speed/files/'))
+}
+
+const checkoutScripts = () => {
+  return gulp.src('src/assets/js/common/checkout6-custom.js')
+    .pipe(babel())
+    .pipe(browserify())
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(gulp.dest('./dist/vtex_speed/files/'))
 }
 
 const pluginsJs = () => {
@@ -129,8 +149,8 @@ function sync(){
       startPath: '/admin/login/',
       proxy: 'https://' + storeName  + '.vtexcommercestable.com.br',
       serveStatic: [{
-          route: '/arquivos',
-          dir: [bases.build]
+          route: ['/files', '/arquivos'],
+          dir: [bases.build + '/../files', bases.build]
       }]
   })
 }
@@ -138,6 +158,10 @@ function sync(){
 const watch = () => {
   gulp.watch(paths.styles.srcWatch, styles).on('change',browserSync.reload);
   gulp.watch(paths.scripts.srcWatch, scripts).on('change',browserSync.reload);
+
+  gulp.watch('src/assets/scss/**/checkout6-custom.scss', checkoutStyles).on('change',browserSync.reload);
+  gulp.watch('src/assets/**/checkout6-custom.js', checkoutScripts).on('change',browserSync.reload);
+
   gulp.watch('src/views/**/*', pugtranspile).on('change',browserSync.reload);
   gulp.watch('src/views/html/*', htmls).on('change',browserSync.reload);
   gulp.watch(paths.imgs.srcWatch, minimg).on('change',browserSync.reload);
@@ -145,12 +169,14 @@ const watch = () => {
 }
 
 //------------------------------ Tasks -----------------------------
-const build = gulp.series(gulp.parallel(sync, minimg, minsvg, styles, scripts, htmls, pugtranspile, watch));
+const build = gulp.series(gulp.parallel(sync, minimg, minsvg, styles, scripts, checkoutStyles, checkoutScripts, htmls, pugtranspile, watch));
 
 // exports.pluginsJs = pluginsJs;
 // exports.clean = clean;
 exports.styles = styles;
 exports.scripts = scripts;
+exports.checkoutStyles = checkoutStyles;
+exports.checkoutScripts = checkoutScripts;
 exports.htmls = htmls;
 exports.pugtranspile = pugtranspile;
 exports.minimg = minimg;
