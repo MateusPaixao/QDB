5a5b8c8f99673f9f5121f598592239db80fe9f3f
Patrick Castro <patrick.gomes@corebiz.com.br>
2019-09-04 16:37:59 -0300

    Configurado header troca de região

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const storeName = 'qbbr';
 
 const bases = {
     src: './src',
-    build: './dist/vtex_speed'
+    build: './dist/vtex_speed/arquivos/'
 }
 
 //----------------------------- Modules ---------------------------------
@@ -14,24 +14,40 @@ const browserify = require('gulp-browserify');
 const browserSync   = require('browser-sync').create();
 // const del = require('del');
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass');
+const svgmin = require('gulp-svgmin');
 const uglify = require('gulp-uglify');
+const rename = require("gulp-rename");
 
 const paths = {
+  imgs: {
+    src: 'src/assets/img/*.{png,gif,jpg}',
+    dest: './dist/assets/img/',
+    srcWatch: 'src/assets/img/*'
+  },
+  svgs: {
+    src: 'src/assets/img/icon/*.svg',
+    dest: './dist/assets/img/icon/',
+    srcWatch: 'src/assets/img/icon/*'
+  },
   styles: {
-    src: 'src/assets/scss/common/*.scss',
-    dest: './dist/assets/css',
-    srcWatch: 'src/assets/scss/**/*.scss',
+    src: ['src/assets/scss/common/*.scss', '!src/assets/scss/common/checkout6-custom.scss'],
+    dest: './dist/assets/css/',
+    qa: './qa/assets/css/',
+    srcWatch: ['src/assets/scss/**/*.scss', '!src/assets/scss/**/checkout6-custom.scss'],
   },
   scripts: {
-    src: 'src/assets/js/common/*.js',
-    dest: './dist/assets/js',
-    srcWatch: 'src/assets/**/*.js',
+    src: ['src/assets/js/common/*.js', 'src/assets/js/common/*.jsx', '!src/assets/js/common/checkout6-custom.js'],
+    dest: './dist/assets/js/',
+    qa: './qa/assets/js/',
+    srcWatch: ['src/assets/**/*.js','src/assets/**/*.jsx', '!src/assets/**/checkout6-custom.js'],
   },
-  htmls: {
-    src: 'src/views/common/**/*.pug',
-    dest: './dist/views/html_templates',
+  markup: {
+    pug: 'src/views/common/*/*.pug',
+    html: 'src/views/html/*.html',
+    dest: './dist/views/html',
   }
 };
 
@@ -42,19 +58,34 @@ const paths = {
 
 const pugtranspile = () => {
   return gulp.src([
-    paths.htmls.src,
-    '!src/views/common/_layouts/*.pug',
-    '!src/views/common/_partials/*.pug',
+    paths.markup.pug,
+    'src/views/common/_layouts/*.pug',
+    'src/views/common/_partials/*.pug',
   ])
     .pipe(pug({
-      pretty: false,
+      pretty: true
     }))
-    .pipe(gulp.dest(paths.htmls.dest));
+    .pipe(gulp.dest(paths.markup.dest));
 }
 const htmls = () => {
-  return gulp.src('src/views/html/*.html')
-    .pipe(gulp.dest(paths.htmls.dest));
+  return gulp.src(paths.markup.html)
+    .pipe(gulp.dest(paths.markup.dest));
 }
+
+const minimg = () => {
+  return gulp.src(paths.imgs.src)
+      .pipe(imagemin())
+      .pipe(gulp.dest(paths.imgs.dest))
+      .pipe(gulp.dest('./dist/vtex_speed/arquivos/'));
+};
+
+const minsvg = () => {
+  return gulp.src(paths.svgs.src)
+      .pipe(svgmin())
+      .pipe(gulp.dest(paths.svgs.dest))
+      .pipe(gulp.dest('./dist/vtex_speed/arquivos/'));
+};
+
 const styles = () => {
   return gulp.src(paths.styles.src)
     .pipe(sass({outputStyle: 'compressed'}))
@@ -63,20 +94,46 @@ const styles = () => {
       cascade: false,
     }))
     .pipe(gulp.dest(paths.styles.dest))
-    .pipe(gulp.dest('./dist/vtex_speed'));
+    .pipe(gulp.dest('./dist/vtex_speed/arquivos/'))
+    .pipe(rename(function (path) {
+      path.basename = "QA-" + path.basename
+    }))
+    .pipe(gulp.dest(paths.styles.qa))
+    .pipe(gulp.dest('./dist/vtex_speed/arquivos/'));
 }
 
 const scripts = () => {
   return gulp.src(paths.scripts.src)
-    .pipe(babel({
-      presets: ["@babel/polyfill", "env"]
-    }))
-/*     .pipe(browserify({
-          transform: ['babelify'],
-      })) */
+    .pipe(babel())
+    .pipe(browserify())
     .pipe(uglify())
     .pipe(gulp.dest(paths.scripts.dest))
-    .pipe(gulp.dest('./dist/vtex_speed'));
+    .pipe(gulp.dest('./dist/vtex_speed/arquivos/'))
+    .pipe(rename(function (path) {
+      path.basename = "QA-" + path.basename
+    }))
+    .pipe(gulp.dest(paths.scripts.qa))
+    .pipe(gulp.dest('./dist/vtex_speed/arquivos/'));
+}
+
+const checkoutStyles = () => {
+  return gulp.src("src/assets/scss/common/checkout6-custom.scss")
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({
+      browsers: ['last 10 versions'],
+      cascade: false,
+    }))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(gulp.dest('./dist/vtex_speed/files/'))
+}
+
+const checkoutScripts = () => {
+  return gulp.src('src/assets/js/common/checkout6-custom.js')
+    .pipe(babel())
+    .pipe(browserify())
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(gulp.dest('./dist/vtex_speed/files/'))
 }
 
 const pluginsJs = () => {
@@ -87,15 +144,15 @@ const pluginsJs = () => {
 }
 
 function sync(){
-  browserSync.init({
+  return browserSync.init({
       open: true,
       https: true,
       host: storeName  + '.vtexlocal.com.br',
       startPath: '/admin/login/',
       proxy: 'https://' + storeName  + '.vtexcommercestable.com.br',
       serveStatic: [{
-          route: '/arquivos',
-          dir: [bases.build]
+          route: ['/files', '/arquivos'],
+          dir: [bases.build + '/../files', bases.build]
       }]
   })
 }
@@ -103,17 +160,29 @@ function sync(){
 const watch = () => {
   gulp.watch(paths.styles.srcWatch, styles).on('change',browserSync.reload);
   gulp.watch(paths.scripts.srcWatch, scripts).on('change',browserSync.reload);
-  gulp.watch('src/views/**/*', htmls, pugtranspile).on('change',browserSync.reload);
+
+  gulp.watch('src/assets/scss/**/checkout6-custom.scss', checkoutStyles).on('change',browserSync.reload);
+  gulp.watch('src/assets/**/checkout6-custom.js', checkoutScripts).on('change',browserSync.reload);
+
+  gulp.watch('src/views/**/*', pugtranspile).on('change',browserSync.reload);
+  gulp.watch('src/views/html/*', htmls).on('change',browserSync.reload);
+  gulp.watch(paths.imgs.srcWatch, minimg).on('change',browserSync.reload);
+  gulp.watch(paths.svgs.srcWatch, minsvg).on('change',browserSync.reload);
 }
 
 //------------------------------ Tasks -----------------------------
-const build = gulp.series(gulp.parallel(sync,styles, scripts, htmls, pugtranspile, watch));
+const build = gulp.series(gulp.parallel(sync, minimg, minsvg, styles, scripts, checkoutStyles, checkoutScripts, htmls, pugtranspile, watch));
 
 // exports.pluginsJs = pluginsJs;
 // exports.clean = clean;
 exports.styles = styles;
 exports.scripts = scripts;
+exports.checkoutStyles = checkoutStyles;
+exports.checkoutScripts = checkoutScripts;
 exports.htmls = htmls;
+exports.pugtranspile = pugtranspile;
+exports.minimg = minimg;
+exports.minsvg = minsvg;
 exports.watch = watch;
 exports.build = build;
 
--- src/assets/js/common/modules/General/Regional/components/_PriceSelect.jsx
+++ src/assets/js/common/modules/General/Regional/components/_PriceSelect.jsx
@@ -25,12 +25,15 @@ class PriceModal extends React.Component {
       return (
         this.setState({
           Session: getCookie("vtex_session"),
-          Region: getCookie("vtexRegion")
+          Region: getCookie("vtexRegion").split(",")[0],
+          LatLng: getCookie("vtexRegion").split(",")[1]
         })
       )
     }
-    
-    document.querySelector(".modalRegional").classList.remove("hidden");
+    document.querySelector(".header__region").classList.add("--active");
+    setTimeout(() => {
+        document.querySelector(".header__region").classList.remove("--active");
+    }, 5000);
     // this.setPosition(this.state.Region, this.state.LatLng);
   }
 
@@ -53,19 +56,19 @@ class PriceModal extends React.Component {
           request.onreadystatechange = () => {
               if (request.readyState === 4) {
                   resolve(JSON.parse(request.response));
-                  document.querySelector(".modalRegional").classList.add("hidden");
                   console.log("Location Up");
               }
           };
 
           request.send(JSON.stringify(data));
       }).then(() => {
-          document.cookie = "vtexRegion=" + Region;
+          document.cookie = "vtexRegion=" + Region + "," + LatLong;
       }).catch(()=>{
           this.setState({
-              Region: "Sudeste"
+              Region: "Sudeste",
+              LatLng: "04551000"
           })
-          document.cookie = "vtexRegion=Sudeste";
+          document.cookie = "vtexRegion=Sudeste,04551000";
       })
   }
   
@@ -77,20 +80,19 @@ class PriceModal extends React.Component {
     this.setState({
       Region: e.split(",")[0],
       LatLng: e.split(",")[1]
+    }, ()=>{      
+      this.setPosition(this.state.Region, this.state.LatLng)
     })
+    // setTimeout(() => {
+    // }, 1000);
   }
 
   render(){
     return (
       <React.Fragment>
-        <div className="modalRegional__overlay"></div>
-        <div className="modalRegional__container">
-          <h3 className="modalRegional__container__title">Qual região você está?</h3>
-          <select className="modalRegional__selection" onChange={e => this.handleChange(e.target.value)} >
-             { this.state.Regions.map(R => <option value={R} key={R.split(",")[1]} selected={R.split(",")[0] == this.state.Region ? "selected" : ""} > {R.split(",")[0]} </option>) }
+          <select className="header__region__selection" value={this.state.Region + "," + this.state.LatLng} onChange={e => this.handleChange(e.target.value)} >
+             { this.state.Regions.map(R => <option className="header__region__selection--option" value={R} key={R.split(",")[1]} > {R.split(",")[0]} </option>) }
           </select>
-          <button className="modalRegional__button" onClick={() => this.setPosition(this.state.Region, this.state.LatLng)}>Estou na Região {this.state.Region}</button>
-        </div>
       </React.Fragment>
     )
   }
