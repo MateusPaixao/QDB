6623d2962f4ac9dca762c145956147f97ff144cf
Regina Santos <regina.santos@corebiz.com.br>
2019-09-04 17:22:20 -0300

    add slide;

--- src/assets/scss/common/layout/quiz_bases/js/main.js
+++ src/assets/scss/common/layout/quiz_bases/js/main.js
@@ -4,16 +4,21 @@ var jsonMap = {
     "claro-medio": new ClaroMedioStrategy()
 }
 
+//variável para controle de steps
+var stepsControl = document.querySelector("[steps-control]");
+
 //recupera todos os elementos do nível principal quiz
 var mainGroup = document.querySelectorAll('[data-main-option]');
 createFirstLevelStructure(mainGroup);
 
-//função que cria instância da classe de acordo com a opção principal escolhida
+//função que cria instância da classe mapeada de acordo com a opção principal escolhida
 function createFirstLevelStructure(mainGroup) {
+    stepsControl.setAttribute("steps-control", "1");
     mainGroup.forEach(element => {
         element.addEventListener('click', function() {
             //variável guarda instância da opção escolhida
             var option = jsonMap[`${this.getAttribute('data-main-option')}`];
+            //pede criação do segundo nível passando a opção
             createSecondLevelStructure(option);
         }) 
     });
@@ -21,31 +26,37 @@ function createFirstLevelStructure(mainGroup) {
 
 //função que cria a segunda estrutura (subtons)
 function createSecondLevelStructure(option) {
+    stepsControl.setAttribute("steps-control", "2");
     var obj = option.getResult();
-    var oldStructure = document.querySelector('[data-main-group]');
+    //obtem estrutura a ser preenchida
     var currentStructure = document.querySelector(`[data-subtone-group=${obj.id}]`);
     var innerElements = currentStructure.querySelectorAll('[data-subtone-option]');
     
     clearSecondStep();
+    //percorre subtons
     innerElements.forEach(element => {
         var subtoneName = element.getAttribute(['data-subtone-option']);
+        //obtem objeto do subtom
         var subtone = obj.subtones[`${subtoneName}`];
         //adiciona conteúdo html respectivo da opção
         element.innerHTML = `<h3>${subtone.name}</h3>
                              <p>${subtone.text}</p>`;
+        element.classList.remove('hide');
         element.addEventListener('click', function() {
+            //pede criação do terceiro nível passando obj subtom escolhido
             createThirdLevelStructure(subtone);
         })
     });
-
-    //oldStructure.classList.add('hide');
     currentStructure.classList.remove('hide');
+    currentSlide(2);
 }
 
 //função que cria a segunda estrutura (resultado de cores)
 function createThirdLevelStructure(subtone) {
+    stepsControl.setAttribute("steps-control", "3");
     var currentStructure = document.querySelector('#result');
     currentStructure.innerHTML = "";
+    //percorre cores sugeridas para o subtom
     subtone.colors.forEach(element => {
         currentStructure.innerHTML += `
                                 <a class="item" href="${element.url}">
@@ -57,21 +68,19 @@ function createThirdLevelStructure(subtone) {
                                 `
     });
     currentStructure.classList.remove('hide');
+    currentSlide(3);
 }
 
+//reinicializa segundo step;
 function clearSecondStep(){
     var subtoneGroup = document.querySelectorAll('[data-subtone-group]');
     var innerElements = document.querySelectorAll('[data-subtone-option]');
 
-    innerElements.forEach(element => {
-        element.innerHTML = "";
-    });
     subtoneGroup.forEach(element => {
-        element.classList.add("hide");
+        element.classList.add('hide');
     });
-}
-
-function clearThirdStep(){
-    var resultGroup = document.querySelector('#result');
-    resultGroup.innerHTML = "";
+    innerElements.forEach(element => {
+        element.innerHTML = '';
+        element.classList.add('hide');
+    });
 }
