4f756d67edb81f1642bc3ffc54b89302d39cb904
Patrick Castro <patrick.gomes@corebiz.com.br>
2019-09-11 13:36:34 -0300

    Produto: Correção em Adicionar a Sacola / Produtos sem idSku com problemas para adicionar

--- src/assets/js/common/QDBCommon-Product.js
+++ src/assets/js/common/QDBCommon-Product.js
@@ -1248,7 +1248,7 @@ let AddToCart = () =>{
     document.querySelector(".product-buy-button .buy-button").addEventListener("click", function(el){
         el.preventDefault();
         let skuId = "";
-        new URL(window.location.href).searchParams.get("idsku") != null ? skuId = document.querySelector(".select-cor-new .group_0 .current").getAttribute("data-idsku") : skuId = new URL(window.location.href).searchParams.get("idsku");
+        new URL(window.location.href).searchParams.get("idsku") == null ? skuId = document.querySelector(".select-cor-new .group_0 .current").getAttribute("data-idsku") : skuId = new URL(window.location.href).searchParams.get("idsku");
         // console.log(skuId);
         el.srcElement.innerHTML = "Adicionando...";
         el.srcElement.style.opacity = ".7";
