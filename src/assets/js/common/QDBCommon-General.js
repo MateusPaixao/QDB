596991e8418d1d966958751b6b3aac8dde45557b
Regina Santos <regina.santos@corebiz.com.br>
2019-09-17 15:01:54 -0300

    form newsletter;

--- src/assets/js/common/QDBCommon-General.js
+++ src/assets/js/common/QDBCommon-General.js
@@ -1779,61 +1779,62 @@ $(document).ready(function(){
 
 });
 
-//form
-
-// function sendNewsletterForm() {
-//     //Form Newsletter
-//     var btn = document.querySelector('#nl_form #submit_button');
-//     $('body.pre-black-week').append("<div class='lightBox_submit'><p><span>X</span>Para que a gente possa te avisar sobre as promoções da Black Week, você precisa preencher todos os dados aceitar receber nossas comunicações.</p></div>");
-//     $('.lightBox_submit p span').click(function () {
-//         $('.lightBox_submit').fadeOut();
-//     });
+function sendBFForm() {
+    //Form newsleter
+    var btn = document.querySelector('#nl_form #submit_button');
+    var lightBox = document.createElement('div');
+    lightBox.classList.add('lightBox_submit');
+    lightBox.innerHTML = '<p><span>X</span>Para que a gente possa te avisar sobre as promoções da Black Week, você precisa preencher todos os dados aceitar receber nossas comunicações.</p>';
+    document.querySelector('#nl_form').appendChild(lightBox);
+    document.querySelector('.lightBox_submit p span').addEventListener('click', function () {
+        document.querySelector('.lightBox_submit').style.display = 'none';
+    });
     
-//     btn.addEventListener("click", function(e) {
-//         e.preventDefault();
-//         //get data
-//         var date = new Date();
-//         var fullDate = `${date.getFullYear()}-${(date.getMonth()<=10?'0':'')}` + `${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${(date.getMinutes()<10?'0':'') + date.getMinutes()}:${date.getSeconds()}`;
-//         var jsonData = JSON.stringify({
-//             'origin': document.querySelector('#bf_origem').value,
-//             'campaign': document.querySelector('#bf_campanha').value,
-//             'date': fullDate,
-//             'name': document.querySelector('#bf_nome').value,
-//             'email': document.querySelector('#bf_email').value,
-//             'acceptEmail': document.querySelector('#bf_autoriza').checked
-//         });
+    btn.addEventListener("click", function(e) {
+        e.preventDefault();
+        //get data
+        var date = new Date();
+        var fullDate = `${date.getFullYear()}-${(date.getMonth()<=10?'0':'')}` + `${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${(date.getMinutes()<10?'0':'') + date.getMinutes()}:${date.getSeconds()}`;
+        var jsonData = JSON.stringify({
+            'origin': document.querySelector('#nl_origem').value,
+            'campaign': document.querySelector('#nl_campanha').value,
+            'date': fullDate,
+            'name': document.querySelector('#nl_nome').value,
+            'email': document.querySelector('#nl_email').value,
+            'acceptEmail': document.querySelector('#nl_autoriza').checked
+        });
 
-//         //set form validation
-//         var filtroEmail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-//         var validUser = document.querySelector("input[name='validation-field']").value == 0;
-//         var validTerms = document.querySelector('#bf_autoriza').checked == true;
-//         var validName = document.querySelector('#bf_nome').value != "";
-//         var validEmail =  filtroEmail.test(document.querySelector('#bf_email').value);
+        //set form validation
+        var filtroEmail = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        var validUser = document.querySelector("input[name='validation-field']").value == 0;
+        var validTerms = document.querySelector('#nl_autoriza').checked == true;
+        var validName = document.querySelector('#nl_nome').value != "";
+        var validEmail =  filtroEmail.test(document.querySelector('#nl_email').value);
 
-//         if (validEmail && validName && validTerms && validUser) {
-//             var XHR = new XMLHttpRequest();
-//             // XHR.open('POST', '//api.vtexcrm.com.br/' + jsnomeLoja + '/dataentities/PS/documents');
-//             XHR.open('POST', '/api/dataentities/PS/documents', true);
-//             // Add the required HTTP header
-//             XHR.setRequestHeader('accept', 'application/vnd.vtex.ds.v10+json');
-//             XHR.setRequestHeader('content-type', 'application/json');
-//             XHR.send(jsonData);
-//             //successful
-//             XHR.addEventListener('load', function (event) {
-//                 alert('Dados confirmados com sucesso!');
-//             });
-//             //error
-//             XHR.addEventListener('error', function (event) {
-//                 alert('Houve um erro, por favor tente novamente.');
-//             });
-//             if (window.location.href.indexOf("?success=1") > -1) {
-//                 $(".form .group-form").remove();
-//                 $(".form .form-success").show();
-//             }
-//         } else {
-//             e.preventDefault();
-//             $('.lightBox_submit').fadeIn();
-//         }
-//     });
-// }
-// window.onload = sendBFData();
+        if (validEmail && validName && validTerms && validUser) {
+            var XHR = new XMLHttpRequest();
+            // XHR.open('POST', '//api.vtexcrm.com.br/' + jsnomeLoja + '/dataentities/PS/documents');
+            XHR.open('POST', '/api/dataentities/PS/documents', true);
+            // Add the required HTTP header
+            XHR.setRequestHeader('accept', 'application/vnd.vtex.ds.v10+json');
+            XHR.setRequestHeader('content-type', 'application/json');
+            XHR.send(jsonData);
+            //successful
+            XHR.addEventListener('load', function (event) {
+                window.location.href = '?success=1';
+            });
+            //error
+            XHR.addEventListener('error', function (event) {
+                alert('Houve um erro, por favor tente novamente.');
+            });
+        } else {
+            e.preventDefault();
+            document.querySelector('.lightBox_submit').style.display = 'block';
+        }
+    });
+    if (window.location.href.indexOf("?success=1") > -1) {
+        document.querySelector('.form .group-form').remove();
+        document.querySelector('.form .form-success').style.display = 'block';
+    }
+}
+window.onload = sendBFForm();
