f5bdf90bf764b0e17a678a7201805e30c01590c6
Regina Santos <regina.santos@corebiz.com.br>
2019-09-18 11:41:10 -0300

    correção de formato de data;

--- src/assets/js/common/QDBCommon-General.js
+++ src/assets/js/common/QDBCommon-General.js
@@ -1785,9 +1785,15 @@ function sendNLForm() {
     var btn = document.querySelector('#nl_form #submit_button');    
     btn.addEventListener("click", function(e) {
         e.preventDefault();
-        //get data
+        //get data 2019-09-17 15:42:31
         var date = new Date();
-        var fullDate = `${date.getFullYear()}-${(date.getMonth()<=10?'0':'')}` + `${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${(date.getMinutes()<10?'0':'') + date.getMinutes()}:${date.getSeconds()}`;
+        var year = `${date.getFullYear()}`;
+        var month = `${(date.getMonth()<=10?'0':'')}` + `${date.getMonth()+1}`;
+        var day = `${(date.getDate()<10?'0':'')}` + `${date.getDate()}`;
+        var hours = `${(date.getHours()<10?'0':'')}` + `${date.getHours()}`;
+        var minutes = `${(date.getMinutes()<10?'0':'')}` + `${date.getMinutes()}`;
+        var seconds = `${(date.getSeconds()<10?'0':'')}` + `${date.getSeconds()}`;
+        var fullDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
         var jsonData = JSON.stringify({
             'origin': 'ECOMM',
             'campaign': 'NEWSLETTER',
