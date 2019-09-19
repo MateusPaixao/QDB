ea84776c927640db40ab36c1ee37567d90f5a7e0
Regina Santos <regina.santos@corebiz.com.br>
2019-09-19 10:06:42 -0300

    consulta de produto por ID;

--- src/assets/js/common/QDB-LP-Super-Especial.js
+++ src/assets/js/common/QDB-LP-Super-Especial.js
@@ -1,10 +1,17 @@
-$( document ).ready(function() {
-  $(".slick-products-list").slick({
-      dots: true,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 2,
-      responsive: [
+const Methods = {
+  init() {
+    Methods.productsSlick();
+    Methods.twentyBanner();
+    Methods.getProductBannerInfo();
+  },
+  productsSlick: () => {
+    $(document).ready(function () {
+      $(".slick-products-list").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        responsive: [
           {
             breakpoint: 550,
             settings: {
@@ -14,11 +21,32 @@ $( document ).ready(function() {
               dots: true
             }
           }
-      ]
+        ]
+      });
     });
-});
-$(".twentytwenty-container").twentytwenty({
-    default_offset_pct: 0.69,
-    orientation: 'horizontal',
-    no_overlay: false
-});   
+  },
+  twentyBanner: () => {
+    $(".twentytwenty-container").twentytwenty({
+      default_offset_pct: 0.69,
+      orientation: 'horizontal',
+      no_overlay: false
+    });
+  },
+  getProductBannerInfo: () => {
+    var productElements = document.querySelectorAll(".panel-product .product-id");
+
+    productElements.forEach((element) => {
+      const productId = element.textContent;
+      const url = 'https://qbbr.vtexcommercestable.com.br/api/catalog_system/pub/products/search/?fq=productId:' + productId;
+
+      fetch(url)
+        .then(res => res.json())
+        .then((product) => {
+          console.log(product[0].productTitle);
+        })
+    });
+  }
+}
+document.addEventListener('DOMContentLoaded', () => {
+  Methods.init();
+})
