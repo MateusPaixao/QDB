c3c95c729f524db7c8e390d1e4c5665c860669eb
Patrick Castro <patrick.gomes@corebiz.com.br>
2019-09-30 09:40:00 -0300

    Configurando indisponivel

--- src/assets/js/common/modules/General/Vitrine/components/_Card.jsx
+++ src/assets/js/common/modules/General/Vitrine/components/_Card.jsx
@@ -41,6 +41,8 @@ class Card extends React.Component{
     return (
       this.setState({
         Avaliable: avaliable
+      }, ()=>{
+        this.setDiscount();
       })
     )
   }
@@ -52,6 +54,11 @@ class Card extends React.Component{
       if(discount != 0){
         discount = Math.round(discount * 100 / this.state.Sku.sellers[0].commertialOffer.ListPrice);
       }
+    } else {
+      discount = 0;
+      this.setState({
+        RawPrice: this.props.info.items.find(sku => sku.sellers[0].commertialOffer.ListPrice != 0 ? sku.sellers[0].commertialOffer.ListPrice.toFixed(2).toString().replace(".", ",") : "")
+      })
     }
     return (
       this.setState({
@@ -103,8 +110,7 @@ class Card extends React.Component{
         Sku: this.props.info.items.find(i => i.itemId == e.dataset.sku),
       }, () => {
         // let listItem = document.querySelectorAll(".cardProduct__config__list");
-        
-        this.setDiscount();
+        console.log(this.props.info);
         this.setBeforePrice();
         this.setAvaliable();
         this.setState({
@@ -317,7 +323,6 @@ class Card extends React.Component{
   }
 
   componentDidMount(){
-    this.setDiscount();
     this.setBeforePrice();
     this.setAvaliable();
     this.setState({SelectedSkuThumb: this.getImgSku(this.state.Sku)});
@@ -346,7 +351,7 @@ class Card extends React.Component{
     const flags = () =>{
       return(
         <React.Fragment>
-          {this.state.Discount != 0 &&
+          {this.state.Discount != 0 && this.state.Avaliable == true &&
           <span className="cardProduct__flag __discount">
               <p className="cardProduct__flag__content">
                 {this.state.Discount + "%"}
@@ -402,23 +407,37 @@ class Card extends React.Component{
             </p>
             <svg className="cardProduct__info--favorite" width="23" height="19" viewBox="0 0 23 19" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.1503 3.53732L11.5 3.87959L11.8497 3.53732L13.3914 2.02851C15.4757 -0.0113849 18.8643 -0.00747146 20.9442 2.02806C23.022 4.06158 23.0154 7.36215 20.9437 9.38962L15.4508 14.7654L11.4951 18.3313C11.4434 18.286 11.3866 18.2361 11.3253 18.1823C11.0157 17.9102 10.5906 17.5356 10.1287 17.1249C9.20159 16.3006 8.1373 15.341 7.55654 14.7726L2.05628 9.38962C-0.0153681 7.36215 -0.0219969 4.06158 2.05583 2.02806C4.13571 -0.00747146 7.52425 -0.0113849 9.60859 2.02851L11.1503 3.53732Z" /></svg>
           </div>
-          <div className="cardProduct__price">
-            <p className={"cardProduct__price__before"}>
-            {
-              this.state.haveBefore != false &&
-              "R$" + this.state.Sku.sellers[0].commertialOffer.ListPrice.toFixed(2).toString().replace(".", ",")
-            }
-            </p>
-            <p className="cardProduct__price__actual">{"R$" + this.state.Sku.sellers[0].commertialOffer.Price.toFixed(2).toString().replace(".", ",")}</p>
-            <p className="cardProduct__price__installment">{"até " + Math.max.apply(Math, this.state.Sku.sellers[0].commertialOffer.Installments.map(function(o) { return o.NumberOfInstallments; })) + "x de R$" + Math.min.apply(Math, this.state.Sku.sellers[0].commertialOffer.Installments.map(function(o) { return o.Value; })).toFixed(2).toString().replace(".", ",") + " sem juros"}</p>
-          </div>
+          {this.state.Avaliable != false ?
+            <div className="cardProduct__price">
+              <p className={"cardProduct__price__before"}>
+              {
+                this.state.haveBefore != false &&
+                "R$" + this.state.Sku.sellers[0].commertialOffer.ListPrice.toFixed(2).toString().replace(".", ",")
+              }
+              </p>
+              <p className="cardProduct__price__actual">{"R$" + this.state.Sku.sellers[0].commertialOffer.Price.toFixed(2).toString().replace(".", ",")}</p>
+              <p className="cardProduct__price__installment">{"até " + Math.max.apply(Math, this.state.Sku.sellers[0].commertialOffer.Installments.map(function(o) { return o.NumberOfInstallments; })) + "x de R$" + Math.min.apply(Math, this.state.Sku.sellers[0].commertialOffer.Installments.map(function(o) { return o.Value; })).toFixed(2).toString().replace(".", ",") + " sem juros"}</p>
+            </div>
+            :
+            <div className="cardProduct__price">
+              <p className="cardProduct__price__actual">{"R$" + this.state.RawPrice}</p>
+            </div>
+          }
         </a>
         {/* <a href={"/" + this.props.info.linkText + "/p?idsku=" + this.state.Sku.itemId} className="cardProduct__link"> */}
           {/* <span className="cardProduct--addToCart"> */}
+          
+          {this.state.Avaliable != false ?
           <span className="cardProduct--addToCart status--standBy" onClick={e => this.addToCart(e.currentTarget)}>
               <svg className="cardProduct--addToCart__bag" width="20" height="24" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18.8779 6.22321C18.8046 5.43449 18.0966 4.83464 17.259 4.85168H14.7489V4.53631C14.7492 2.03134 12.6006 0.000399137 9.94984 3.99499e-05C9.94963 3.99499e-05 9.94941 3.99499e-05 9.9492 3.99499e-05C7.36904 -0.0102225 5.26862 1.95802 5.25779 4.39623C5.25758 4.44293 5.25817 4.48962 5.2595 4.53631V4.85291H2.74878C1.93116 4.85876 1.25334 5.45317 1.18619 6.22321L0.0167767 19.9383C-0.0877588 20.9886 0.299876 22.0295 1.07749 22.7865C1.81873 23.5618 2.8738 24.0029 3.97953 23.9997H16.0327C17.1408 24.0136 18.2003 23.5707 18.9347 22.7865C19.6662 22.0032 20.0466 20.9818 19.9954 19.9383L18.8779 6.22321ZM6.32086 4.53631C6.29403 2.61379 7.92153 1.03475 9.95597 1.00941C11.9904 0.984058 13.6614 2.52205 13.6882 4.44457C13.6886 4.47515 13.6886 4.50573 13.6882 4.53631V4.85291H6.32086V4.53631ZM18.1541 22.0475C17.6184 22.6231 16.8445 22.9502 16.0334 22.9441H3.97697C2.39256 22.9385 1.11301 21.7201 1.11904 20.2229C1.11936 20.1452 1.1232 20.0677 1.13056 19.9903L2.30248 6.27519C2.30573 6.03731 2.51246 5.84694 2.76419 5.85001C2.77785 5.85017 2.79145 5.85094 2.805 5.85222H5.31641V7.80374C5.31641 8.09504 5.56628 8.33118 5.87455 8.33118C6.18283 8.33118 6.4327 8.09504 6.4327 7.80374V5.85463H13.8V7.80615C13.8 8.09745 14.0499 8.33359 14.3581 8.33359C14.6664 8.33359 14.9163 8.09745 14.9163 7.80615V5.85463H17.4277C17.687 5.84853 17.9068 6.03351 17.9302 6.27755L19.1021 19.9926C19.0331 20.7573 18.6999 21.4793 18.1541 22.0475Z" /></svg>
               <p className="cardProduct--addToCart__cta">Adicionar à Sacola</p>
           </span>
+          :
+          <span className="cardProduct--letMeKnow status--standBy" onClick={e => this.letMeKnow(e.currentTarget)}>
+              <svg  className="cardProduct--letMeKnow__mail" width="24" height="17" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.098 0h17.698c1.692 0 2.993 1.301 2.993 2.993v10.671c0 1.692-1.301 3.123-2.993 3.123H3.098c-1.692 0-2.994-1.431-2.994-3.123V2.994C.104 1.3 1.406 0 3.098 0zM2.057 1.822l8.328 6.897c.781.65 2.212.65 3.123 0l8.329-6.897c-.26-.26-.65-.52-1.041-.52H3.098c-.39 0-.781.26-1.041.52zm20.43 1.301L14.42 9.76c-1.431 1.171-3.643 1.171-4.945 0L1.406 3.123v10.541c0 .911.78 1.692 1.692 1.692h17.698c.91 0 1.692-.78 1.692-1.692V3.124z" fill="#FDFDFD"/></svg>
+              <p className="cardProduct--letMeKnow__cta">Avise-me quando chegar</p>
+          </span>
+          }
         {/* </a> */}
       </div>
     );
