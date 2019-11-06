39ec3ed68aaaddcd2f47e76d553ff6eb83fa6c74
Patrick Castro <patrick.gomes@corebiz.com.br>
2019-11-06 19:02:32 -0300

    Header e Footer Pretos

--- src/assets/js/common/modules/Search/search-index.js
+++ src/assets/js/common/modules/Search/search-index.js
@@ -1,12 +1,12 @@
-import oldScripts from './_departament-old';
+import oldScripts from './_search-old';
 import SmartResearch from './_vtex-smartResearch'
-import BuildSearch from './_category'
+import BuildSearch from './_search'
 import {isInViewport} from '../../global/global-index'
 const Methods = {
     init() {
         isInViewport();
         BuildSearch.init();
-        // oldScripts.init();
+        oldScripts.init();
         // SmartResearch.init();
     }
 }
