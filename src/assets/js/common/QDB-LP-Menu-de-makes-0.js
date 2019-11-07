997569e258283846cb9da8f1a2350f0f92a62b73
Regina Santos <regina.santos@corebiz.com.br>
2019-11-07 19:15:17 -0300

    modal-scheduling incompleto;

--- src/assets/js/common/QDB-LP-Menu-de-makes-0.js
+++ src/assets/js/common/QDB-LP-Menu-de-makes-0.js
@@ -2,7 +2,11 @@ import Siema from "./global/vendor/siema-slider/siema.min.js"
 
 function setOptionsCarousel() {
 	const options = new Siema({
-		perPage: 3
+		perPage: {
+			0: 1,
+			420: 2,
+			768: 3
+		}
 	});
 	
 	const prev = document.querySelector('.options-carousel__controls .prev');
@@ -10,5 +14,24 @@ function setOptionsCarousel() {
 	prev.addEventListener('click', () => options.prev());
 	next.addEventListener('click', () => options.next());
 }
+function setSchedulingModal() {
+	const modal = document.querySelector('.modal-scheduling');
+	const optionContainer = modal.querySelector('.modal-scheduling__main__content');
+
+	document.querySelectorAll('.options-section__item__button').forEach(item => {
+		item.addEventListener('click', event => {
+			event.preventDefault();
+			optionContainer.innerHTML = item.closest('.options-section__item').outerHTML;
+			modal.classList.add('show');
+		});
+	});
+	document.querySelectorAll('.modal-scheduling__close').forEach(item => {
+		item.addEventListener('click', event => {
+			modal.classList.remove('show');
+		});
+	});
+
+}
 
 setOptionsCarousel();
+setSchedulingModal();
