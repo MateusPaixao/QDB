8bbb7c97e075ca93e4f6728a5cc30f0e780423e0
Leonardo Prieto <leonardo.santos@grupoboticario.com.br>
2019-11-21 18:56:07 -0300

    update na função que monta os links do modal

--- src/assets/js/common/modules/Home/Components/InfoBar/modal_infoBar.js
+++ src/assets/js/common/modules/Home/Components/InfoBar/modal_infoBar.js
@@ -12,13 +12,22 @@ const Methods = {
         document.querySelector('.infoBar__render').appendChild(_modalContent);
         for(let i = 0; i < _itens.length; i++){
             var _itemInfo =  _itens[i];
-            _itemInfo.addEventListener("click", openInfoModal);
-             
-        }  
+            // var _itemHref =  _itens[i].querySelector('.__content a');
+            // var _itemLink = "<a class='_ctaModal' href='" +
+            // _itemHref.href +
+            // "'>aproveite!</a>";
+            _itemInfo.addEventListener("click", openInfoModal); 
+        }
         function openInfoModal() { 
             document.body.classList.add("__modal--on");
-            _modalContent.innerHTML = "<span class='__closeModal'>X</span>" + this.innerHTML;
-        }    
+            _modalContent.innerHTML = "<span class='__closeModal'>X</span>" + 
+            this.innerHTML; 
+            document.querySelector('.__closeModal').addEventListener("click", closeInfoModal);
+        }
+        function closeInfoModal() { 
+            document.body.classList.remove("__modal--on");
+        }
+
     }
 }
 export default {
