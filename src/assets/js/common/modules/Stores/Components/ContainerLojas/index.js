b4be216e3265b10c2ecd7e12227bb93d5d27b938
Lucas Wenceslau <lucascetef@gmail.com>
2020-03-20 12:41:43 -0300

    ajustes nossas lojas

--- src/assets/js/common/modules/Stores/Components/ContainerLojas/index.js
+++ src/assets/js/common/modules/Stores/Components/ContainerLojas/index.js
@@ -4,6 +4,20 @@ import { CheckSVG } from '../SVGs';
 const Container = ({ Stores }) => {
   React.useEffect(() => { }, []);
 
+  function copyCoupon(ev) {
+    const couponCode = ev.target.previousElementSibling;
+    couponCode.select();
+    document.execCommand("copy");
+
+    const couponTarget = ev.target;
+    couponTarget.textContent = "copiado!";
+
+    setTimeout(() => {
+      console.log(couponTarget)
+      couponTarget.textContent = "copiar";
+    }, 3000);
+  }
+
   return (
     // console.log(Stores)
     Stores.map((store, i) => (
@@ -30,13 +44,13 @@ const Container = ({ Stores }) => {
         </div>
 
         {store.make && (
-          <div className="store__address">
+          <div className="store__address store__especial">
             <p className="store__hasMake">
-              {' '}
               {/* <CheckSVG />  */}
-              <b className="store__percent">%</b> Cupom da loja: <b className="store__coupon"> {store.coupon} </b>
+              Cupom da loja: <input type="text" value={store.coupon} className="store__coupon" />
+              <button class="store__button" onClick={ev => copyCoupon(ev)}> copiar </button>
             </p>
-            {/* <a className="store__agendar" href="/servicos">
+            {/* <a className= href="/servicos">
               Agendar make
             </a> */}
           </div>
